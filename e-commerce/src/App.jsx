58aa79c2c13e2deb3858e731da87c6a20c1e735c
Hemanth1372 <hemanthguntuku18@gmail.com>
2025-11-28 15:07:55 +0530

    Updated Orders

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -2,6 +2,7 @@ import Homepage from './pages/HomePage'
 import { Routes, Route } from 'react-router';
 import './App.css';
 import CheckoutPage from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={<OrdersPage />} />
       </Routes>
     </>
   )
